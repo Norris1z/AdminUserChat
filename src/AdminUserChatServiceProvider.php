0d91c2e33d9be76af5ba5dbfea7fb56a16d8f75e
Norris Oduro <norrisjibril@gmail.com>
2017-02-25 18:45:44 +0000

    Add Package Service Provider

---
+++ src/AdminUserChatServiceProvider.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Norris1z\AdminUserChat;
+
+use Illuminate\Support\ServiceProvider;
+
+class AdminUserChatServiceProvider extends ServiceProvider
+{
+    /**
+     *  Bootstrap the application services
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+
+    /**
+     *  Register the application services
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+}
